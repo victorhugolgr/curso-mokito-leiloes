a6a67eee37ccaa8393ae3c1c8f4319092b695680
victor.hugo <victor.hugo@tre-pa.jus.br>
2021-01-12 14:22:45 -0300

    03 Escrevendo um teste com Mockito

---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,51 @@
+package br.com.alura.leilao.service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+
+class FinalizarLeilaoServiceTest {
+
+	private FinalizarLeilaoService service;
+
+	@Mock
+	private LeilaoDao leilaoDao;
+
+	@BeforeEach
+	public void BeforeEach() {
+		MockitoAnnotations.initMocks(this);
+		this.service = new FinalizarLeilaoService(leilaoDao);
+	}
+
+	@Test
+	void deveriaFinalizarUmLeilao() {
+		List<Leilao> leiloes = leiloes();
+		
+		service.finalizarLeiloesExpirados();
+	}
+
+	private List<Leilao> leiloes() {
+		List<Leilao> lista = new ArrayList<Leilao>();
+
+		Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Fulano"));
+		
+		Lance primeiro = new Lance(new Usuario("Beltrano"), new BigDecimal("600"));
+		Lance segundo = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+		
+		leilao.propoe(primeiro);
+		leilao.propoe(segundo);
+		lista.add(leilao);
+		
+		return lista;
+	}
+}
